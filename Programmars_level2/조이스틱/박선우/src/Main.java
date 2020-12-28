771d53fdf6ead96064bc2bf06c63fa6d052ce9e6
romaakk <romaakk@naver.com>
2020-12-28 21:11:18 +0900

    쪼이스틱 박선우

---
+++ Programmars_level2/조이스틱/박선우/src/Main.java
@@ -0,0 +1,30 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Solution sol = new Solution();
+		
+		String name = "AABAAAAAAAAABBB";
+		System.out.println(name+": 11");
+		System.out.println(sol.solution(name));
+		
+
+		name = "BBAAABA";
+		System.out.println(name+": 7");
+		System.out.println(sol.solution(name));
+		
+		name = "CANAAAAANAN";
+		System.out.println(name+": 48");
+		System.out.println(sol.solution(name));
+		
+		name = "BAABAAAAAB";
+		System.out.println(name+": 10");
+		System.out.println(sol.solution(name));
+		
+		
+
+	}
+
+}
